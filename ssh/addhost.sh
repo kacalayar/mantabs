d920eb029194e9fd1e3d367d4269efc32764073f
kenncode <96781765+kenDevXD@users.noreply.github.com>
2022-08-06 11:39:28 +0700

    Update addhost.sh

--- ssh/addhost.sh
+++ ssh/addhost.sh
@@ -12,11 +12,11 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl ipinfo.io/ip | grep $MYIP )
+
 clear
 read -rp "Domain/Host : " -e domain
 echo "IP=$domain" >>/var/lib/crot/ipvps.conf
 rm -rf /etc/xray/domain
 echo $domain > /etc/xray/domain
 certv2ray
-
-
